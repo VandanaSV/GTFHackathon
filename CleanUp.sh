92042b9e4c72b7f57bbc0f62e20debbbe64636f8
vandana.sv <vandana.sv@allianz.com>
2021-10-25 12:25:54 +0530

    latest

--- CleanUp.sh
+++ CleanUp.sh
@@ -2,8 +2,6 @@ echo "--------------------------------------------------------------------------
 echo "######### Cleanup ##########"
 echo "-------------------------------------------------------------------------------------------------------"
 
-rm -r Application
-rm -r libraries/application-jar/*
 rm -r code-coverage-report.zip
 rm -r exec/*
 rm -r code-coverage-report/*
